5966ab6c7f6dff5accd1ee02baafd86604eae640
Maxence Younsi <mx.yns@outlook.fr>
2023-05-11 11:23:39 +0200

    Initial commit

---
+++ bmp/bmp.py
@@ -0,0 +1,43 @@
+import operator
+from enum import Enum
+from pyshark.packet.layers.xml_layer import XmlLayer
+
+
+class IntEnum(Enum):
+    def __cmp__(self, other):
+        return self.value == other
+
+    def __eq__(self, other):
+        return self.value == other
+
+
+class MessageType(IntEnum):
+    RouteMonitoring = 0
+    StatisticsReport = 1
+    PeerDown = 2
+    PeerUp = 3
+    Initiation = 4
+
+
+class PeerType(IntEnum):
+    GlobalInstance = 0
+    RDInstance = 1
+    LocalInstance = 2
+    LocRibInstance = 3
+
+
+class BmpPacket:
+
+    def __init__(self, capture_sequence: int, frame_sequence: int, frame: int, packet: XmlLayer):
+        self.capture_sequence = capture_sequence
+        self.frame_sequence = frame_sequence
+        self.frame = frame
+        self.packet = packet
+        self.type = MessageType(int(self.packet.type))
+
+    # transparent wrapper to avoid using packet.packet.attr
+    def __getattr__(self, item):
+        if item not in self.__dict__:
+            return operator.attrgetter(item)(self.packet)
+        else:
+            return getattr(self, item)
---
+++ tests/test_bmp.py
@@ -0,0 +1,167 @@
+import unittest
+
+import pyshark
+
+from bmp import bmp
+from bmp.bmp import BmpPacket
+
+TESTDATA_FILENAME = "/home/taayoma5/frr-ribout-testing-20230511_1048.pcap"
+TSHARK_PATH = "/home/taayoma5/wireshark/build/run/tshark"
+
+
+class BMP(unittest.TestCase):
+    file_path: str = None
+    pcap: pyshark.FileCapture = None
+    bmp: list[BmpPacket] = None
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.file_path = TESTDATA_FILENAME
+        cls.pcap = pyshark.FileCapture(TESTDATA_FILENAME, tshark_path=TSHARK_PATH)
+        cls.bmp = list()
+
+        seq = 0
+        for frame_id, frame in enumerate(cls.pcap):
+            if (packets := frame.get_multiple_layers("bmp")) is not None and len(packets) > 0:
+                for frame_seq, packet in enumerate(packets):
+                    cls.bmp.append(
+                        BmpPacket(capture_sequence=seq, frame_sequence=frame_seq, frame=frame_id, packet=packet))
+                    seq += 1
+
+        print(f"BMP Packet count: {seq + 1}")
+        # this is disgustingly inefficient
+        types: dict[bmp.MessageType, int] = {
+            msg_type.name: len(list(filter(lambda bmp_packet: int(bmp_packet.packet.type) == msg_type, cls.bmp))) for
+            msg_type in bmp.MessageType}
+        print(types)
+
+    # ensure that preprocessing didn't duplicate packets
+    def test_indices(self):
+        # check if list is going from 0 to len(lst) monotonically, incr of 1
+        def _assert_monotone(lst: list):
+            assert len(set(lst)) == len(lst)
+            assert list(range(len(lst))) == lst
+
+        capture_sequences = list(map(lambda p: p.capture_sequence, self.bmp))
+        _assert_monotone(capture_sequences)
+
+    # ensure that the bmp version is the same for the same sessions
+    def test_version(self):
+
+        fail = False
+        sessions = dict()
+        for packet in self.bmp:
+            if packet.type == bmp.MessageType.Initiation:
+                frame = self.pcap[packet.frame]
+                session_id = (frame.ip.dst, frame.ip.src, frame.tcp.port, frame.tcp.dstport)
+                bmp_version = int(packet.version)
+                if sessions.get(session_id) is None:
+                    sessions[session_id] = (packet.capture_sequence, bmp_version)
+
+                # if version changed
+                if sessions[session_id][1] != bmp_version:
+                    print(
+                        f"Version changed from {sessions[session_id][1]} to {bmp_version} on session {str(session_id)}")
+                    fail = True
+
+        assert not fail
+
+    # summarize peer up/down state and count ignored messages (received before peer up / after peer down)
+    def test_peerup(self):
+
+        # peer stores
+        # peer ids as key are tuple of (IP, RD, Type)
+        # values are dicts with id, type, state, stats etc.
+        peers: dict[
+            tuple[str, str, int],
+            dict[str, any]
+        ] = dict()
+
+        vrfs: dict[
+            tuple[str, str, int],
+            dict[str, any]
+        ] = dict()
+
+        # get a peer from one of the local peer stores
+        def _get_peer(peer_type: bmp.PeerType, peer_id):
+            store = vrfs if peer_type == bmp.PeerType.LocRibInstance else peers
+            return store.setdefault(peer_id, {
+                "id": peer_id,
+                "type": peer_type,
+                "type_name": peer_type.name,
+                "state": None,
+                "state_msgs": list(),
+                "stats": dict()
+            })
+
+        # increment a stat for a peer, create it if missing
+        def _incr_stat(peer, stat_name: str):
+            peer["stats"][stat_name] = peer["stats"].setdefault(stat_name, 0) + 1
+
+        # play the bmp pcap
+        for packet in self.bmp:
+
+            # if a state message this is a new peer state
+            packet_type = packet.type
+
+            # ignore initiation messages, not per-peer header
+            if packet_type == bmp.MessageType.Initiation:
+                continue
+
+            peer_type = bmp.PeerType(int(packet.peer_type))
+            peer_ip = packet.peer_ip_addr if "peer_ip_addr" in packet.field_names else packet.peer_ipv6_addr
+            peer_id = (peer_ip, packet.peer_distinguisher, peer_type.value)
+            peer = _get_peer(peer_type, peer_id)
+            peer_state = peer["state"]
+
+            # got a peer state message, update peer state
+            if packet_type in [bmp.MessageType.PeerUp, bmp.MessageType.PeerDown]:
+                if peer_state == packet_type:
+                    _incr_stat(peer, f"{packet_type.name}_duplicate")
+                    print(f"peer {peer_id} duplicate state {peer_state}")
+                else:
+                    peer["state_msgs"].append(packet.capture_sequence)
+                    peer["state"] = packet_type
+                    _incr_stat(peer, packet_type.name)
+                    print(f"{peer['type_name']} {peer_id} changed state {peer_state} -> {packet_type}")
+
+            # got any other message
+            else:
+                not_up = peer_state != bmp.MessageType.PeerUp
+                _incr_stat(peer, f"{packet_type.name}_ignored" if not_up else packet_type.name)
+
+        # TODO pretty print the summary router-like
+        print(peers)
+        print(vrfs)
+
+    # ensure that the peer type is never 0 when the peer RD is not zero and vice-versa
+    def test_peer_type(self):
+
+        fail = False
+        for packet in self.bmp:
+            if not all(field in packet.field_names for field in ["peer_type", "peer_distinguisher"]):
+                continue
+
+            can_rd = int(packet.peer_type) != bmp.PeerType.GlobalInstance
+            need_rd = int(packet.peer_type) in [bmp.PeerType.RDInstance, bmp.PeerType.LocalInstance]
+            has_rd = packet.peer_distinguisher != "00:00:00:00:00:00:00:00"
+            if (has_rd and not can_rd) or (need_rd and not has_rd):
+                print(f"Packet {packet.capture_sequence} in frame {packet.frame + 1} at "
+                      f"{packet.frame_sequence}/{len(self.pcap[packet.frame].get_multiple_layers('bmp'))} "
+                      f"has invalid type / rd combination")
+                print(f"Message type: {packet.type}, "
+                      f"peer type: {packet.packet.peer_type}. "
+                      f"out: {packet.packet.peer_flags_adj_rib_out}, "
+                      f"post: {packet.packet.peer_flags_post_policy}")
+                fail = True
+
+        assert not fail
+
+    # TODO summary of received message for each type of monitoring (in - loc - out) with prefix and peer address
+
+    def tearDown(self) -> None:
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
